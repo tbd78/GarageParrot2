3b731da16b83e4fd89bc983da4acd4115832a5ff
momo63 <mtambadou@outlook.com>
2024-04-29 16:47:33 +0200

    Ajout système de routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,9 @@
+import Route from "./route.js";
+
+//Définir ici vos routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),
+];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Garage";
